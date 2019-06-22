4c3c40bd620a1dfcf479210abf1a270e35c0071d
homdx <37062532+homdx@users.noreply.github.com>
2019-06-22 23:23:39 +0300

    store fix - not emty archieve

--- store-cache.sh
+++ store-cache.sh
@@ -2,9 +2,9 @@
 
 if [ -z "$DISABLECACHE" ] ; \
     then echo 'Now not archieve - cached files make are null size. If you not need cache build with: --build-arg DISABLECACHE=something'; \
-    touch $TRAVIS_BUILD_DIR/cargo.7z ; \
-    touch $TRAVIS_BUILD_DIR/deltachat-core-rust.7z ; \
-    touch $TRAVIS_BUILD_DIR/rustup-arc.7z ; \
+    7z a -bsp1 -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -r $TRAVIS_BUILD_DIR/cargo.7z store-cache.sh; \
+    7z a -bsp1 -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -r $TRAVIS_BUILD_DIR/deltachat-core-rust.7z store-cache.sh ; \
+    7z a -bsp1 -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -r $TRAVIS_BUILD_DIR/rustup-arc.7z store-cache.sh ; \
     else echo Cache are disabled = $DISABLECACHE; \
     # Store cache and upload  \
     echo Store cache and upload;
