e1fcf8e072fd6c04b3792ee4b8c8ccd122f5b21f
homdx <37062532+homdx@users.noreply.github.com>
2019-06-23 21:45:31 +0300

    debug echo

--- store-cache.sh
+++ store-cache.sh
@@ -6,11 +6,12 @@ if [ -z "$DISABLECACHE" ] ; \
     7z a -bsp1 -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -r $TRAVIS_BUILD_DIR/cargo.7z store-cache.sh; \
     7z a -bsp1 -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -r $TRAVIS_BUILD_DIR/deltachat-core-rust.7z store-cache.sh ; \
     7z a -bsp1 -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -r $TRAVIS_BUILD_DIR/rustup-arc.7z store-cache.sh ; \
+    ls -la ; \
     echo desktop node rust>$TRAVIS_BUILD_DIR/deltachat-source-info.txt ; \
-    cd $TRAVIS_BUILD_DIR/deltachat-desktop && git log -1 >>$TRAVIS_BUILD_DIR/deltachat-source-info.txt ; cd .. ; \
-    cd $TRAVIS_BUILD_DIR/deltachat-node && git log -1 >>$TRAVIS_BUILD_DIR/deltachat-source-info.txt ; cd deltachat-code-rust ; \
-    git log -1 >>$TRAVIS_BUILD_DIR/deltachat-source-info.txt ; cd .. ; cd .. ; \
-    7z a -bsp1 -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -r  deltachat-snapshot-sources.7z deltachat-source-info.txt  ; \
+    cd $TRAVIS_BUILD_DIR/deltachat-desktop && echo 11 && ls -la && git log -1 >>$TRAVIS_BUILD_DIR/deltachat-source-info.txt ; cd .. ; \
+    ls -la && echo 12 && cd $TRAVIS_BUILD_DIR/deltachat-node && ls -la && git log -1 >>$TRAVIS_BUILD_DIR/deltachat-source-info.txt ; cd deltachat-core-rust ; \
+    ls -la && echo 12 && git log -1 >>$TRAVIS_BUILD_DIR/deltachat-source-info.txt ; cd .. ; cd .. ; \
+    ls -la && echo 14 && 7z a -bsp1 -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -r  deltachat-snapshot-sources.7z deltachat-source-info.txt  ; \
     cat deltachat-source-info.txt ; \
     else echo Cache are disabled = $DISABLECACHE; \
     # Store cache and upload  \
