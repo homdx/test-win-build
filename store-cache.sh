1b5595a7a67c9c0747784f8ebce5b6259a8d5a55
homdx <37062532+homdx@users.noreply.github.com>
2019-06-23 17:40:23 +0300

    fix path for sources file arc

--- store-cache.sh
+++ store-cache.sh
@@ -18,5 +18,5 @@ if [ -z "$DISABLECACHE" ] ; \
     cd /c/Users/travis/.cargo ; 7z a -bsp1 -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -r $TRAVIS_BUILD_DIR/cargo.7z * ; \
     cd $TRAVIS_BUILD_DIR/deltachat-node/deltachat-core-rust/  ; 7z a -bsp1 -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -r $TRAVIS_BUILD_DIR/deltachat-core-rust.7z * ; \
     cd /c/Users/travis/.rustup/ ; 7z a -bsp1 -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -r $TRAVIS_BUILD_DIR/rustup-arc.7z * ; \
-    cd $TRAVIS_BUILD_DIR/ ; 7z a -bsp1 -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -r deltachat-snapshot-sources.7z deltachat-node/ delatachat-desktop/ ; \
+    cd $TRAVIS_BUILD_DIR/ ; 7z a -bsp1 -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -r deltachat-snapshot-sources.7z deltachat-node/ deltachat-desktop/ ; \
     fi
