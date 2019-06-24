99bbd32fd098c24a16cda206036aa3e02ede9e2c
homdx <37062532+homdx@users.noreply.github.com>
2019-06-24 17:21:18 +0300

    show cargo version

--- make-build-cache.sh
+++ make-build-cache.sh
@@ -28,6 +28,7 @@ if [ -z "$DISABLECACHE" ] ; \
     cp -pv cargo-config2 /c/Users/travis/.cargo/config ; \
     export PATH=/c/Users/travis/.cargo/bin:$PATH ; \
     echo start cargo update ; \
+    cargo --version ; \
     cargo update --verbose ; \
     set +ex ; \
     else echo Cache are disabled = $DISABLECACHE build full version with cache; \
