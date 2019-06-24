e08377dc0ac3ab207a64ad72fa2a40180c0f1ca3
homdx <37062532+homdx@users.noreply.github.com>
2019-06-24 21:04:41 +0300

    cargo update after then copy config

--- make-build-cache.sh
+++ make-build-cache.sh
@@ -25,10 +25,12 @@ if [ -z "$DISABLECACHE" ] ; \
     echo "${DOT_HASH2}  ${DOT_FILE2}" | sha256sum -c ; \
     time -p 7z x -y ${DOT_FILE2} ; rm ${DOT_FILE2} ; cd .. ; \
     cd $TRAVIS_BUILD_DIR/ ; git clone --bare https://github.com/rust-lang/crates.io-index.git ; \
-    cp -pv cargo-config2 /c/Users/travis/.cargo/config ; \
     export PATH=/c/Users/travis/.cargo/bin:$PATH ; \
     echo start cargo update ; \
     cargo --version ; \
+    cargo update --verbose ; \
+    echo cargo updated now copy config ; \
+    cp -pv cargo-config2 /c/Users/travis/.cargo/config ; \
     cargo update --verbose ; \
     set +ex ; \
     else echo Cache are disabled = $DISABLECACHE build full version with cache; \
