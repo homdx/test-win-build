0152613319005b3965d3bd32fc7d33d3564e530a
HomDX <uqgvhom3dxto@bk.ru>
2019-06-26 00:47:49 +0300

    stage echo

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 function make_node {
+echo make node
 cd $TRAVIS_BUILD_DIR && ls -la && export PATH=/c/Users/travis/.cargo/bin:/c/Program\ Files/nodejs:$PATH && cd deltachat-node && npm install && npm link
 }
 
 function make_desktop {
+echo make desktop
 cd $TRAVIS_BUILD_DIR && export PATH=/c/Users/travis/.cargo/bin:/c/Program\ Files/nodejs:$PATH && cd deltachat-desktop && export PATH=/c/Users/travis/.cargo/bin:/c/Program\ Files/nodejs:$PATH && npm link deltachat-node && npm install && echo build deltachat dekstop windows && npm run build && echo test deltachat windows && npm run test
 }
 
-function ok_exit{
+function ok_exit {
 echo 'All done without error \:-\)'
 exit 0
 }
