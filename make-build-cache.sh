bae26b36176195911670874c37eb60e4ceec3b5d
homdx <37062532+homdx@users.noreply.github.com>
2019-06-26 10:15:29 +0300

    Release 0.105.0 version

--- make-build-cache.sh
+++ make-build-cache.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-export DOT_VERSION=cached-0.104b.0
+export DOT_VERSION=cached-0.105.0
 export DOT_PATH=https://github.com/homdx/test-win-build/releases/download
 export DOT_FILE1=cargo.7z
 export DOT_FOLDER1="/c/Users/travis/.cargo"
-export DOT_HASH1=2f07c168ac68c60d2be3625551c1f8ce56c6c5ab17c0f24e1e09d4826c8026addb2ef21bcead8c276bc8ce531a1ebb47bef266edbb540b011be31c41221ffe67
+export DOT_HASH1=73ec5d45ced651b7d48ba49d2ee7f01bca11a1ed619118189e7608e91ebb5911a3b74d604ff78ee0e6b2676b923aa82c39a3fc55224e84ef0d615d4e74634bfe
 export DOT_FILE2=rustup-arc.7z
 export DOT_FOLDER2="/c/Users/travis/.rustup"
-export DOT_HASH2=3aaa93485b0de6b1fafdc7a1d11d230280cd8181700e48beb5da1d80647a592d14bb923e4fd4cb67c5bc608ebcde4f643859edd68bf67bf761ffbc01b8601a5c
+export DOT_HASH2=7002c15d5f7febf8fdecea5fd1e0a4e5665492efb8a516526c4ace68b72eb9ae5761eb9d0ae2ba110168bb9a0d8c063b867dc525582b7cf491bd75b9d37a9fa5
 export DOT_FILE3=cargo-indexes.7z
-export DOT_HASH3=34f79331f3865eb2e7523cda76b0469822feab86287759aa6d86fa06926146ca3cc00e4982ac8e87f8c98ed5018d06f8edbd379fe376dd5b65a1f2d7052599ee
+export DOT_HASH3=93e0e55175c3e0c96cdf6a88bc4556626822395a18214efbb6ef73ef18dfca903f8612fd9d56e6707bbd8b793d217b5755abf8339476e3ede7e05e61e605a547
 export DOT_FILE4=deltachat-snapshot-sources.7z
-export DOT_HASH4=deb4fc2f1f9fc97663a521a17192d4bbd2300c387ae1f88a99f551836639ee890fb9017379456e7bb8317104a5f2e341591166a34a87ab421f97c4eb10e341e5
+export DOT_HASH4=7ea5b939ab3ab9b546e8e3c6cb4c3fe2ab3680ba691e51df1452aa69c1b178a5e82f86e060e1bb787e0babfe002cc60108c8befd274bc49528bdafc002225c67
 
 if [ -z "$DISABLECACHE" ] ; \
     then echo 'Now enable Cached files for rust. If you not need cache build with: --build-arg DISABLECACHE=something'; \
