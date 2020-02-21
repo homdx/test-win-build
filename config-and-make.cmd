9117ed1cc66e69fc547f63f5092b0658cb5cb2d3
homdx <37062532+homdx@users.noreply.github.com>
2020-02-21 17:20:04 +0300

    Update config-and-make.cmd

--- config-and-make.cmd
+++ config-and-make.cmd
@@ -1,2 +1,5 @@
-echo configure vs and qt
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x86 configure.bat -debug -recheck-all -I "C:/Program Files (x86)/OpenSSL-Win32/include" -L "C:/Program Files (x86)/OpenSSL-Win32/lib" -prefix C:/Qt515 -opensource -confirm-license -nomake examples -nomake tests -skip qtwebengine
+echo on
+echo configure vs
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x86
+echo configure qt
+configure.bat -debug -recheck-all -I "C:/Program Files (x86)/OpenSSL-Win32/include" -L "C:/Program Files (x86)/OpenSSL-Win32/lib" -prefix C:/Qt515 -opensource -confirm-license -nomake examples -nomake tests -skip qtwebengine
