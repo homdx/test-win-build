0626fc55a6005803f309014d28b660b28b3bc467
homdx <37062532+homdx@users.noreply.github.com>
2020-02-21 20:05:19 +0300

    Create make-qt.cmd

---
+++ make-qt.cmd
@@ -0,0 +1,9 @@
+echo on
+echo configure vs
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x86
+echo start build
+jom.exe
+echo end build
+echo start install
+jom.exe install /s
+echo done install
