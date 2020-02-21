48539bd95c6a9b0c1d5128f57211d2a5be1ce150
homdx <37062532+homdx@users.noreply.github.com>
2020-02-21 20:22:53 +0300

    only config

--- config-and-make.cmd
+++ config-and-make.cmd
@@ -3,9 +3,3 @@ echo configure vs
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x86
 echo configure qt
 configure.bat -debug -recheck-all -I "C:/Program Files (x86)/OpenSSL-Win32/include" -L "C:/Program Files (x86)/OpenSSL-Win32/lib" -prefix C:/Qt515 -opensource -confirm-license -nomake examples -nomake tests -skip qtwebengine
-echo start build
-jom.exe
-echo end build
-echo start install
-jom.exe install /s
-echo done install
